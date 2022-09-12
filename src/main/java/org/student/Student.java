b38c8be4febc6900028d8040c97118756a8e0de4
Rishabh Gist <damyrachit@gmail.com>
2022-09-12 14:48:15 -0400

    added fields, getters and setters, equals, toString and hashCode

--- src/main/java/org/student/Student.java
+++ src/main/java/org/student/Student.java
@@ -1,4 +1,11 @@
 package org.student;
 
+import lombok.Data;
+
+@Data
 public class Student {
+    private byte rollNumber;
+    private String firstName;
+    private String lastName;
+    private byte currentYearOfStudent;
 }
